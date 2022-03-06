28c583142edf66622a983a6a0922d28a65a6c479
Alfonso Embid-Desmet <mail@alfon.net>
2022-03-06 15:08:16 -0800

    simplify for now\

--- functions/src/__tests__/setup-tests.ts
+++ functions/src/__tests__/setup-tests.ts
@@ -1,4 +1,8 @@
+import dotenv from "dotenv";
 import functions from "firebase-functions-test";
+
+// Setting up ENV variables
+dotenv.config({ path: './.env.test' });
 
 // @ts-ignore
 global.firebaseTest = functions({
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,23 +1,13 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import { validateAndGetPayload } from "./validate-and-get-payload";
-import * as dotenv from "dotenv";
 import { validateApiVersion } from "./validate-api-version";
 import { requestErrorHandler } from "./error-handler";
-import path from "path";
-
-const ENV_FILES_PATH = "./";
-const ENV_FILE = path.join(ENV_FILES_PATH, process.env.NODE_ENV === "test" ? ".env.test" : ".env");
-
-dotenv.config({
-  path: ENV_FILE
-});
-
 admin.initializeApp();
 
 const SHARED_SECRET = process.env.REVENUECAT_SHARED_SECRET as string;
 const EVENTS_LOCATION = process.env.REVENUECAT_EVENTS_LOCATION as string;
-const EXTENSION_VERSION = process.env.EXTENSION_VERSION as string;
+const EXTENSION_VERSION = process.env.EXTENSION_VERSION || "1.0.0";
 
 interface BodyPayload {
   api_version: string;
