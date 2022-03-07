1d2a2422a4f3559c04d714fc1caf4a8242d63040
Alfonso Embid-Desmet <mail@alfon.net>
2022-03-07 08:57:50 -0800

    remove functions dir

--- functions/src/log-message.ts
+++
@@ -1,5 +0,0 @@
-import * as functions from "firebase-functions";
-
-export const logMessage = (message: string | {message: string, code: number}, level: "info" | "error" ="info") => {
-    functions.logger[level](message, { structuredData: true });
-}
