a17e9e83f14407dce07bb37457c0a25974dda99d
Alfonso Embid-Desmet <mail@alfon.net>
2022-03-09 11:30:51 -0800

    add tests for when we dont want to save either events or customer info

--- functions/src/__tests__/events.test.ts
+++ functions/src/__tests__/events.test.ts
@@ -25,18 +25,40 @@ describe("events", () => {
         expect(mockedResponse.getHeaders()).toEqual({ 'X-EXTENSION-VERSION': validPayload.api_version });
     });
 
-    it("saves the event in the configured events collection", (done) => {
+    it("saves the event in the configured events collection", async () => {
         const mockedResponse = getMockedResponse(expect, () => Promise.resolve())(200, {}) as any;
         
         const mockedRequest = getMockedRequest(createJWT(60, validPayload, "test_secret")) as any;
 
         api.handler(mockedRequest, mockedResponse);
 
-        setTimeout(async () => {
-            const doc = await admin.firestore().collection("revenuecat_events").doc("uuid").get();
-            expect(doc.data()).toEqual(validPayload.event);
-            done();
-        }, 500);
+        await timeout(300);
+
+        const doc = await admin.firestore().collection("revenuecat_events").doc("uuid").get();
+        expect(doc.data()).toEqual(validPayload.event);
+    });
+
+    it("doesn't save the event if the REVENUECAT_EVENTS_COLLECTION setting is not set", async () => {
+        jest.resetModules();
+        const originalProcessEnv = process.env;
+        process.env = {
+            ...originalProcessEnv,
+            REVENUECAT_EVENTS_COLLECTION: ""
+        }
+
+        const { handler } = require("../index")
+
+        const mockedResponse = getMockedResponse(expect, () => Promise.resolve())(200, {}) as any;
+        const mockedRequest = getMockedRequest(createJWT(60, {...validPayload, event: {...validPayload.event, id: "not_save_this"}}, "test_secret")) as any;
+
+        handler(mockedRequest, mockedResponse);
+
+        await timeout(300);
+
+        const doc = await admin.firestore().collection("revenuecat_events").doc("not_save_this").get();
+        expect(doc.data()).toEqual(undefined);
+
+        process.env = originalProcessEnv;
     });
 
     it("saves the customer_info in the customer collection", async () => {
@@ -61,5 +83,28 @@ describe("events", () => {
 
         const updatedDoc = await admin.firestore().collection("revenuecat_customers").doc("miguelcarranza").get();
         expect(updatedDoc.data()).toEqual({...validPayload.customer_info, ...additionalCustomerInfo });
+    });
+
+    it("doesn't save the event if the REVENUECAT_CUSTOMERS_COLLECTION setting is not set", async () => {
+        jest.resetModules();
+        const originalProcessEnv = process.env;
+        process.env = {
+            ...originalProcessEnv,
+            REVENUECAT_CUSTOMERS_COLLECTION: ""
+        }
+
+        const { handler } = require("../index")
+
+        const mockedResponse = getMockedResponse(expect, () => Promise.resolve())(200, {}) as any;
+        const mockedRequest = getMockedRequest(createJWT(60, {...validPayload, customer_info: {...validPayload.customer_info, original_app_user_id: "not_save_this"}}, "test_secret")) as any;
+
+        handler(mockedRequest, mockedResponse);
+
+        await timeout(300);
+
+        const doc = await admin.firestore().collection("revenuecat_customers").doc("not_save_this").get();
+        expect(doc.data()).toEqual(undefined);
+
+        process.env = originalProcessEnv;
     });
 });
