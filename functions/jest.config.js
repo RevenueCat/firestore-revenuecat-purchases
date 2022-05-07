3654577c1e02d1b884018103ab3074d7fe1370c3
dackers86 <ackers86@hotmail.com>
2022-05-07 14:44:55 +0100

    manually added eventarc to jestmodules

--- functions/jest.config.js
+++ functions/jest.config.js
@@ -1,13 +1,16 @@
 // this config includes typescript specific settings
 // and if you're not using typescript, you should remove `transform` property
 module.exports = {
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
-    testRegex: 'src/__tests__/.+\.test.tsx?$',
-    testPathIgnorePatterns: ['lib/', 'node_modules/'],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    testEnvironment: 'node',
-    rootDir: 'src',
-    setupFiles: ["<rootDir>/__tests__/setup-tests.ts"]
-}
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "/src/__tests__/.+.test.tsx?$",
+  testPathIgnorePatterns: ["lib/", "node_modules/"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+  testEnvironment: "node",
+  setupFiles: ["<rootDir>/src/__tests__/setup-tests.ts"],
+  moduleNameMapper: {
+    "firebase-admin/eventarc":
+      "<rootDir>/node_modules/firebase-admin/lib/eventarc/index.js",
+  },
+};
