59e5b59c649082f54e643079a203b5a3d5966024
Alfonso Embid-Desmet <alfperata@gmail.com>
2023-05-11 17:10:19 -0700

    bump version

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -28,7 +28,7 @@ const CUSTOMERS_COLLECTION = process.env.REVENUECAT_CUSTOMERS_COLLECTION as
 const SET_CUSTOM_CLAIMS = process.env.SET_CUSTOM_CLAIMS as
   | "ENABLED"
   | "DISABLED";
-const EXTENSION_VERSION = process.env.EXTENSION_VERSION || "0.1.7";
+const EXTENSION_VERSION = process.env.EXTENSION_VERSION || "0.1.8";
 
 const getCustomersCollection = ({
   firestore,
